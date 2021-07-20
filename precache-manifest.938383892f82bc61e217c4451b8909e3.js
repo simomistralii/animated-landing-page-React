078ba6ecc10078dd3b1fa9f45275efb99ee3b54f
Sagar naik <sagarnaikg00@gmail.com>
2021-07-20 14:34:10 +0530

    Updates

---
+++ precache-manifest.938383892f82bc61e217c4451b8909e3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd334da0ba61ed4559489f909b332b1",
+    "url": "/animated-landing-page-React/index.html"
+  },
+  {
+    "revision": "068d50f908b6d661ed63",
+    "url": "/animated-landing-page-React/static/css/main.7333e602.chunk.css"
+  },
+  {
+    "revision": "ac6d0d605161766b4262",
+    "url": "/animated-landing-page-React/static/js/2.7f7e25d0.chunk.js"
+  },
+  {
+    "revision": "10017bfb08c3f52cf1c70a46d09c7c96",
+    "url": "/animated-landing-page-React/static/js/2.7f7e25d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068d50f908b6d661ed63",
+    "url": "/animated-landing-page-React/static/js/main.bd440825.chunk.js"
+  },
+  {
+    "revision": "f863141665138ce17399",
+    "url": "/animated-landing-page-React/static/js/runtime-main.d08e60fe.js"
+  },
+  {
+    "revision": "f9a44e1ccb9f44cd4de1da8fe7f6f4d8",
+    "url": "/animated-landing-page-React/static/media/arrow-left.f9a44e1c.svg"
+  },
+  {
+    "revision": "dc372a07777e87891aaac087cfbe60ad",
+    "url": "/animated-landing-page-React/static/media/arrow-right.dc372a07.svg"
+  },
+  {
+    "revision": "24e8d8f29296438b099443772c822a1f",
+    "url": "/animated-landing-page-React/static/media/arrow-up.24e8d8f2.svg"
+  },
+  {
+    "revision": "e84c205576ce51cc6a90653c49df11f0",
+    "url": "/animated-landing-page-React/static/media/curology-min.e84c2055.png"
+  },
+  {
+    "revision": "379bad944ff3f73142989e87c5b24773",
+    "url": "/animated-landing-page-React/static/media/lumin-min.379bad94.png"
+  },
+  {
+    "revision": "5f9e30771ee4dbbad1689af16cb49028",
+    "url": "/animated-landing-page-React/static/media/yourspace-min.5f9e3077.png"
+  }
+]);
